dfbf5b6c268b39e1a035c0f396584a4b19014900
malinga-PW <pixelwave.lk@gmail.com>
2026-06-22 12:39:17 +0530

    Update availability form with room type selector for Memos Inn

--- components/SearchSection/index.js
+++ components/SearchSection/index.js
@@ -7,9 +7,7 @@ import "react-datepicker/dist/react-datepicker.css";
 const SearchSection = (props) => {
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setendDate] = useState(new Date());
-
-    const [searchActive, setsearchState] = useState(false);
-
+    const [roomType, setRoomType] = useState('');
 
     const SubmitHandler = (e) => {
         e.preventDefault()
@@ -18,31 +16,6 @@ const SearchSection = (props) => {
     const ClickHandler = () => {
         window.scrollTo(10, 0);
     }
-
-    const [adult, setCount] = useState(0);
-    const adultincrementCount = () => {
-        setCount(adult + 1);
-    };
-    const adultdecrementCount = () => {
-        setCount(adult - 1);
-    };
-
-    const [child, setChild] = useState(0);
-    const childincrementCount = () => {
-        setChild(child + 1);
-    };
-    const childdecrementCount = () => {
-        setChild(child - 1);
-    };
-
-    const [room, setroom] = useState(0);
-    const roomincrementCount = () => {
-        setroom(room + 1);
-    };
-    const roomdecrementCount = () => {
-        setroom(room - 1);
-    };
-
 
     return (
         <div className={`wpo-select-section ${props.svClass}`}>
@@ -65,46 +38,17 @@ const SearchSection = (props) => {
                                         </div>
                                     </div>
                                     <div className="select-sub">
-                                        <div className="form-group tourist-group">
-                                            <div className="tourist-group-wrap">
-                                                <div className="tourist-inner" onClick={() => setsearchState(!searchActive)}>
-                                                    <i className="fi flaticon-user"></i>
-                                                    <ul>
-                                                        <li><input disabled type="text" id="adults" value={adult}/>Adults
-                                                        </li>
-                                                        <li><input disabled type="text" id="children" value={child}/>
-                                                            Children</li>
-                                                        <li><input disabled type="text" id="rooms" value={room}/> Room</li>
-                                                    </ul>
-                                                    <i className={`ti-angle-down ${searchActive ? "rotate" : ""}`}></i>
-                                                </div>
-                                                <div className={`tourist-dropdown ${searchActive ? "active" : ""}`}>
-                                                    <div className="tourist-item">
-                                                        <span>Adults</span>
-                                                        <div className="tourist-item-group">
-                                                            <button type="button" onClick={adultdecrementCount} id="adults_dec">-</button>
-                                                            <input disabled id="adults_val" value={adult} type="text" />
-                                                            <button type="button" onClick={adultincrementCount} id="adults_inc">+</button>
-                                                        </div>
-                                                    </div>
-                                                    <div className="tourist-item">
-                                                        <span>Children</span>
-                                                        <div className="tourist-item-group">
-                                                            <button type="button" onClick={childdecrementCount} id="children_dec">-</button>
-                                                            <input disabled id="children_val" value={child} type="text" />
-                                                            <button type="button" onClick={childincrementCount} id="children_inc">+</button>
-                                                        </div>
-                                                    </div>
-                                                    <div className="tourist-item">
-                                                        <span>Rooms</span>
-                                                        <div className="tourist-item-group">
-                                                            <button type="button" onClick={roomdecrementCount} id="rooms_dec">-</button>
-                                                            <input disabled id="rooms_val" value={room} type="text" />
-                                                            <button type="button" onClick={roomincrementCount} id="rooms_inc">+</button>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
+                                        <div className="input-group">
+                                            <select value={roomType} onChange={(e) => setRoomType(e.target.value)} className="form-control">
+                                                <option value="">Select Room Type</option>
+                                                <option value="economy-double">Economy Double Room - $28/night</option>
+                                                <option value="deluxe-queen">Deluxe Queen Room - $32/night</option>
+                                                <option value="dormitory-mixed">Mixed Dormitory (4-Bed) - $16/bed</option>
+                                                <option value="dormitory-female">Female Dormitory (4-Bed) - $16/bed</option>
+                                                <option value="dormitory-male">Male Dormitory (4-Bed) - $16/bed</option>
+                                                <option value="deluxe-balcony">Deluxe Queen with Balcony - $36/night</option>
+                                            </select>
+                                            <i className="fi flaticon-user"></i>
                                         </div>
                                     </div>
                                     <div className="select-sub">
